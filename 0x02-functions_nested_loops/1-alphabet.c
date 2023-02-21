72a475a5a4377f1a749ff8ab77d9f506496ff689
adilfettach <adilerrai24@gmail.com>
2023-02-21 12:41:58 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 174; i < 200; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
