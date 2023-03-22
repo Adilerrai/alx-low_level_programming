1b74806c8ebf087f8b1a57eaa48a7ba1cf883088
adilfettach <adilerrai24@gmail.com>
2023-03-22 03:37:57 -0700

    first task fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include "function_pointers"
-
+#include "function_pointers.h"
 /**
  * print_name - function to print name
  *
@@ -14,5 +13,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name && f)
 f(name);
 }
