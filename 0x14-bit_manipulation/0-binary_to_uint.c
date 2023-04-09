63f72021b64590d161a8a616f2198de133bc9394
adilfettach <adilerrai24@gmail.com>
2023-04-09 15:04:03 -0700

    First task done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 0;
+	int i = 0;
+
+	if (b == 0)
+		return (0);
+	for (i = 0 ; (b[i] =='0') || (b[i] == '1'); i++)
+	{
+		number  <<= 1;
+		number += b[i] - '0';
+	}
+	return (number);
+}
