be67d3a5bf9978bffb7ab9e60353d42ff3a131d4
adilfettach <adilerrai24@gmail.com>
2023-04-10 06:34:23 -0700

    last task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * function that returns the value of a bit at a given index.
+ *  * get_bit- function that returns the value of a bit at a given index.
  *   *
  *    * @n: the number the index is the index of the bit
- *     *
+ *     *@index: is the index, starting from 0 of the bit you want to get
  *      *  Return: If an error occurs - -1.
  *           Otherwise - The value of bit at index.
  */
