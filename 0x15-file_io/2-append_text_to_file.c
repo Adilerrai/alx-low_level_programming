34f00bf7284e06e9536b50927f8c770269159318
adil errai <adilerrai24@gmail.com>
2023-05-08 06:40:40 -0700

    fixed and tested task 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,11 +29,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	w = write(fd, text_content, len);
 	if (w == -1)
-	{	
+	{
 		close(fd);
 	return (-1);
 	}
-	
+
 	close(fd);
 	return (1);
 }
