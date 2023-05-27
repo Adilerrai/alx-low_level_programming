727945e452cec5e85ecc417ff331f86eb8de40dd
adil errai <adilerrai24@gmail.com>
2023-05-26 21:21:19 -0700

    last chance

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,13 +5,13 @@
  *   *
  *    * Return: If big-endian - 0.
  *     *         If little-endian - 1.
- *      */
+ */
 int get_endianness(void)
 {
 		int num = 1;
 			char *endian = (char *)&num;
 
-				if (*endian == 1)
+		if (*endian == 1)
 							return (1);
 
 					return (0);
