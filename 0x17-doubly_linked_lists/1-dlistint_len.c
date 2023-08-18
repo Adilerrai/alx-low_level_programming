ada7b5b957a0d1eb82768765fff4141a625a4343
adilfettach <adilerrai24@gmail.com>
2023-08-18 10:49:20 -0700

    task2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,16 +2,16 @@
 
 
 
-
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
-  	
-	while (dlistint != NULL)
- 	{
- 		count++;
- 		dlistin_t = dlistint_t->next;
+	size_t count = 1;
+
+	if (!h)
+		return (0);
+	while (h->next)
+	{
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
